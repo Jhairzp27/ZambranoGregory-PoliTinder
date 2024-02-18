a03ffc7c426e7e6ec111820b78b403e2f9410bf4
Jhairzp_27 <jhairzp27@hotmail.com>
2024-02-18 17:52:51 -0500

    Add Table Persona

--- Script/DML_PoliTinder.sql
+++ Script/DML_PoliTinder.sql
@@ -1,6 +1,27 @@
 -- DELETE FROM CargoPersona WHERE NombreCargo = "Admin";
-INSERT OR IGNORE INTO CargoPersona(IdCargo, NombreCargo, NombrePersona, FechaModificacion)VALUES (1, "Admin", "2024-02-16", "Jhair");
-INSERT OR IGNORE INTO CargoPersona(IdCargo, NombreCargo, NombrePersona, FechaModificacion)VALUES (3, "Admin", "2024-02-16", "Luck");
-INSERT OR IGNORE INTO CargoPersona(IdCargo, NombreCargo, NombrePersona, FechaModificacion)VALUES (4, "Admin", "2024-02-16", "Susan");
-INSERT OR IGNORE INTO CargoPersona(IdCargo, NombreCargo, NombrePersona, FechaModificacion)VALUES (1, "Admin", "2024-02-16", "Susan");
-INSERT OR IGNORE INTO CargoPersona(IdCargo, NombreCargo, NombrePersona, FechaModificacion)VALUES (2, "User", "2024-02-18", "Eliana");
+INSERT OR IGNORE INTO CargoPersona(NombreCargo, FechaModificacion)VALUES ("Admin", "2024-02-16");
+INSERT OR IGNORE INTO CargoPersona(NombreCargo, FechaModificacion)VALUES ( "User", "2024-02-18");
+
+INSERT OR IGNORE INTO SexoPersona(TipoSexo, FechaModificacion) VALUES("Hombre",CURRENT_TIMESTAMP);
+INSERT OR IGNORE INTO SexoPersona(TipoSexo, FechaModificacion) VALUES("Mujer",CURRENT_TIMESTAMP);
+
+INSERT OR IGNORE INTO SexoPersona(TipoSexo,FechaCreacion)
+    SELECT "Hombre", CURRENT_TIMESTAMP
+    WHERE NOT EXISTS(SELECT 1 FROM SexoPersona WHERE TipoSexo = "Nombre");
+
+INSERT OR IGNORE INTO SexoPersona(TipoSexo,FechaCreacion)
+    SELECT "Mujer", CURRENT_TIMESTAMP
+    WHERE NOT EXISTS(SELECT 1 FROM SexoPersona WHERE TipoSexo = "Mujer");
+
+INSERT INTO Persona (IdPersonaCargo, IdPersonaSexo, Cedula, Nombres, Apellidos)
+SELECT 1,1, '1719524783', 'Jhair'
+FROM PersonaSexo
+ORDER BY RANDOM()
+LIMIT 1;
+
+INSERT INTO Persona (IdPersonaCargo, IdPersonaSexo, Cedula, Nombres, Apellidos)
+SELECT 1,2, '1438720961', 'Eli'
+FROM PersonaSexo
+ORDER BY RANDOM()
+LIMIT 1;
+
