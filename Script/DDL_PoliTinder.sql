e14d8ba5e955e4c2031adfe272cca0c280c4c8b1
Jhairzp_27 <jhairzp27@hotmail.com>
2024-02-18 17:08:30 -0500

    Add table SexoPersona

--- Script/DDL_PoliTinder.sql
+++ Script/DDL_PoliTinder.sql
@@ -1,14 +1,30 @@
+-- 1. Definición de Tablas
+DROP TABLE CargoPersona;
+DROP TABLE SexoPersona;
 
-CREATE TABLE IF NOT EXISTS CargoPersona(
-    IdCargo INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS CargoPersona (
+    IdCargo INTEGER PRIMARY KEY AUTOINCREMENT,
     NombreCargo TEXT,
-    FechaModificacion TEXT,
-    NombrePersona Text
-    CONSTRAINT controlAdministradores CHECK(
-        (NombreCargo = "Admin" AND
-        (SELECT COUNT(*) FROM CargoPersona WHERE NombreCargo = "Administrador") <=2) 
-        OR
-        (NombreCargo <> "Admin")
-    )
+    FechaModificacion TEXT
 );
+
+-- 2. Crear Trigger para Control de Administradores
+CREATE TRIGGER IF NOT EXISTS controlAdministradores
+    BEFORE INSERT ON CargoPersona
+    FOR EACH ROW
+    WHEN (
+        NEW.NombreCargo = "Admin" AND
+        (SELECT COUNT(*) FROM CargoPersona WHERE NombreCargo = "Admin") >= 2
+    )
+    BEGIN
+        SELECT RAISE(ABORT, "Excede el límite de dos administradores");
+    END;
+
+CREATE TABLE IF NOT EXISTS SexoPersona(
+    IdSexo INTEGER PRIMARY KEY AUTOINCREMENT,
+    TipoSexo TEXT NOT NULL UNIQUE,
+    FechaCreacion DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FechaModificacion DATE
+);
+
 
